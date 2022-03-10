2bd58e8b2aacd565842692fbbd5bdff5fdccb582
Samirza0912 <samirza@code.edu.az>
2022-03-10 17:44:33 +0400

    feat: month/season task created

--- t1hw/Program.cs
+++ t1hw/Program.cs
@@ -6,7 +6,28 @@ namespace t1hw
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("enter month's name :");
+            string month = Console.ReadLine();
+            if ((month == "january") || (month == "february") || (month == "december"))
+            {
+                Console.WriteLine("winter");
+            }
+            else if ((month == "march") || (month == "april") || (month == "may"))
+            {
+                Console.WriteLine("spring");
+            }
+            else if ((month == "june") || (month == "july") || (month == "august"))
+            {
+                Console.WriteLine("summer");
+            }
+            else if ((month == "september") || (month == "october") || (month == "november"))
+            {
+                Console.WriteLine("autumn");
+            }
+            else
+            {
+                Console.WriteLine("invalid");
+            }
         }
     }
 }
